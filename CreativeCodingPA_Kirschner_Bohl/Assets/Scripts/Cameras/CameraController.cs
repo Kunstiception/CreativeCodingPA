c580c79528149324e041ab1aca7bd0712299e6a8
Kunstiception <lukas-bohl.de@outlook.de>
2024-06-11 23:16:06 +0200

    Kameras (vorerst) funktionstüchtig gemacht

--- CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/Cameras/CameraController.cs
+++ CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/Cameras/CameraController.cs
@@ -1,11 +1,10 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
- 
-
     // Reference to the player controller
     public PlayerController playerController;
 
@@ -18,23 +17,59 @@ public class CameraController : MonoBehaviour
     // Reference to the animator
     private Animator _animator;
 
+    // Reference to the idleCam
+    private CinemachineFreeLook _idleCam;
+
+    // Reference to the moveCam
+    private CinemachineVirtualCamera _moveCam;
+
+    // A bool to descibe the current state of idleCam's X axis and to prevent it from being reset on every frame
+    private bool _isMoveCamFixed;
+
 
  // Start is called before the first frame update
  void Start()
     {
+        // Get the animator component
         _animator = GetComponent<Animator>();
+
+        // Get idle camera
+        _idleCam = GameObject.Find("State-Driven Camera").GetComponentInChildren<CinemachineFreeLook>();
+
+        // Get move camera
+        _moveCam = GameObject.Find("State-Driven Camera").GetComponentInChildren<CinemachineVirtualCamera>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if the player does not move, switch to idle cam
         if (playerController.verticalInput == 0 && playerController.horizontalInput == 0)
         {
             _animator.SetBool("isMoving", false);
+            // reset the free loook cameras x axis value to 0, so it is centered behind the player when the camera is entered
+            if (_isMoveCamFixed == false)
+            {
+                ResetIdleCam();
+            }
+            
+            
         }
+        // else switch to move cam and set _isMoveCamFixed to false
         else
         {
             _animator.SetBool("isMoving", true);
+            _isMoveCamFixed = false;
         }
     }
+
+    // Resets the idleCam's X value to 0, so it starts behind the player not anywhere else, also reset the corresponding bool so this only happens once
+    public void ResetIdleCam()
+    {
+        _idleCam.m_XAxis.Value = 0;
+        _isMoveCamFixed = true;
+
+    }
+
 }
