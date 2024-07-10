6ea02681f3dcd16b47589b380feb3f8b89b205a6
Kunstiception <lukas-bohl.de@outlook.de>
2024-07-10 12:07:55 +0200

    Lightsources Abschaltung
    Lightsources aus bereits passierten Arealen werden nach dem Reload abgeschalten (Lichtobjekt + Skript).

--- CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/GameManagement/LightsBlackout.cs
+++ CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/GameManagement/LightsBlackout.cs
@@ -5,45 +5,19 @@ using UnityEngine;
 
 public class LightsBlackout : MonoBehaviour
 {
-    // All lightsources in an array
-    public Light[] Lights;
-
-    // The time between the blackouts
-    public float waitTime;
-
-    // The index of the current lightsource within the array
-    private int _lightsIndex;
-
-    // The timer that dictates when the next lightsource will be shut off
-    private float _blackoutTimer;
+    public GameObject[] lightParents;
 
     private void Start()
     {
-        // Index is set to -1, so no lightsource will be shut off at the beginning of the application
-        _lightsIndex = -1;
-    }
-
-    void Update()
-    {
-        
-        // https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
-        // On update the timer adds Time.deltaTime
-        _blackoutTimer += Time.deltaTime;
-
-        
-        // If the timer reaches/surpasses the wait time and the current lightsIndex is within the bounds of the array, call the Blackout function and reset the timer by subtracting the wait time
-        if (_blackoutTimer >= waitTime && _lightsIndex<=Lights.Length)
+        // Switch off the lightsources of the areas the player has already passed through
+        if(Progression.Instance != null && Progression.Instance.progress > 0)
         {
-            Blackout();
-            _blackoutTimer = _blackoutTimer - waitTime;
+            foreach (Transform light in lightParents[Progression.Instance.progress - 1].transform.GetComponentInChildren<Transform>())
+            {
+                light.GetComponentInChildren<Light>().enabled = false;
+                light.GetComponentInChildren<LightSourceHealing>().gameObject.SetActive(false);
+            }
         }
-
-    }
-
-    // Shuts off the lightsource that is linked to the current lightsIndex
-    void Blackout()
-    {
-        _lightsIndex++;
-        Lights[_lightsIndex].enabled = false; 
+        
     }
 }
