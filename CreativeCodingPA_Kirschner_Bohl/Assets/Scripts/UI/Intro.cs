8634da49548f3f5b703a5db74af88ba18d0bba28
Kunstiception <lukas-bohl.de@outlook.de>
2024-07-25 11:28:53 +0200

    Intro und Outro verbessert
    Intro und Outro besitzt nun Untertitel. Dazu wurde die Audioaufnahmen zugeschnitten, damit die Anzeige der Untertitel passt. Audio und Text werden über eine neue angelegte Coroutine gesteuert, die in der alten Coroutine liegen.

--- CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/UI/Intro.cs
+++ CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/UI/Intro.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class Intro : MonoBehaviour
 {
-    public AudioClip introAudio;
+    public AudioClip[] introAudios;
+    public string[] subtitles;
     public AnimationClip fadeAway;
+    private TMP_Text _text;
+    private Image _textBox;
     private Animator _animator;
     private Animator _animatorBlackScreen;
     private AudioSource _audioSource;
@@ -18,6 +22,9 @@ public class Intro : MonoBehaviour
         _animator = GetComponent<Animator>();
         _animatorBlackScreen = GameObject.Find("BlackScreenForeground").GetComponent<Animator>();
         _audioSource = GetComponent<AudioSource>();
+        _text = GameObject.Find("Subtitles").GetComponent<TMP_Text>();
+        _textBox = GameObject.Find("Text_Background").GetComponent<Image>();
+        _textBox.gameObject.SetActive(false);
         
         StartCoroutine(WaitForAudio());
     }
@@ -25,18 +32,35 @@ public class Intro : MonoBehaviour
     IEnumerator WaitForAudio()
     {
        _animatorBlackScreen.SetTrigger("FadeAway");
-
         yield return new WaitForSeconds(fadeAway.length);
 
-        _audioSource.PlayOneShot(introAudio, 1);
-        
-        yield return new WaitForSeconds(introAudio.length);
+        _textBox.gameObject.SetActive(true);
+
+        // https://discussions.unity.com/t/coroutines-inside-coroutines/733010/2
+        yield return PlayAudioAndSetText(0);
+
+        yield return PlayAudioAndSetText(1);
+
+        yield return PlayAudioAndSetText(2);
+
+        yield return PlayAudioAndSetText(3);
+
+        yield return PlayAudioAndSetText(4);
+
+        _text.gameObject.SetActive(false);
+        _textBox.gameObject.SetActive(false);
 
         _animator.SetTrigger("FadeAway");
-
         yield return new WaitForSeconds(fadeAway.length);
 
         SceneManager.LoadScene(2);
 
+    }
+
+    private IEnumerator PlayAudioAndSetText(int index)
+    {
+        _audioSource.PlayOneShot(introAudios[index], 1);
+        _text.text = subtitles[index];
+        yield return new WaitForSeconds(introAudios[index].length);
     }
 }
--- CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/UI/Outro.cs
+++ CreativeCodingPA_Kirschner_Bohl/Assets/Scripts/UI/Outro.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Outro : MonoBehaviour
 {
-    public AudioClip outroAudio;
+    public AudioClip[] outroAudios;
+    public string[] subtitles;
     public AnimationClip fadeAway;
     public AnimationClip fadeToBlack;
+    private TMP_Text _text;
+    private Image _textBox;
     private Animator _animator;
-    private Animator _fadeToBlackAnimator;
+    private Animator _animatorBlackScreen;
     private AudioSource _audioSource;
+    private Animator _fadeToBlackAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,8 @@ public class Outro : MonoBehaviour
         _animator = GetComponent<Animator>();
         _fadeToBlackAnimator = GameObject.Find("FadeToBlack").GetComponent<Animator>();
         _audioSource = GetComponent<AudioSource>();
+        _text = GameObject.Find("Subtitles").GetComponent<TMP_Text>();
+        _textBox = GameObject.Find("Text_Background").GetComponent<Image>();
         StartCoroutine(WaitForAudio());
     }
 
@@ -30,17 +38,29 @@ public class Outro : MonoBehaviour
     IEnumerator WaitForAudio()
     {
         _animator.SetTrigger("FadeAway");
-
         yield return new WaitForSeconds(fadeAway.length);
-        _audioSource.PlayOneShot(outroAudio, 1);
 
+        _textBox.gameObject.SetActive(true);
 
-        yield return new WaitForSeconds(outroAudio.length);
+        yield return PlayAudioAndSetText(0);
+
+        yield return PlayAudioAndSetText(1);
+        
+        _text.gameObject.SetActive(false);
+        _textBox.gameObject.SetActive(false);
+
         _fadeToBlackAnimator.SetTrigger("FadeAway");
-
         yield return new WaitForSeconds(fadeToBlack.length);
+
         SceneManager.LoadScene(0);
 
 
+    }
+
+    private IEnumerator PlayAudioAndSetText(int index)
+    {
+        _audioSource.PlayOneShot(outroAudios[index], 1);
+        _text.text = subtitles[index];
+        yield return new WaitForSeconds(outroAudios[index].length);
     }
 }
